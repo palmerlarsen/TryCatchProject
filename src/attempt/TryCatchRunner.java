574b68e1ccb95b0c411bf5809ed0cb756b3ec094
palmerlarsen <pman52@gmail.com>
2014-01-21 08:01:04 -0700

    pushed to online database

---
+++ src/attempt/TryCatchRunner.java
@@ -0,0 +1,39 @@
+package attempt;
+
+import javax.swing.JOptionPane;
+
+public class TryCatchRunner
+{
+
+	
+	public static void main(String[] args)
+	{
+		
+		int testNumber = 15;
+		double testFloatingPoint = 0.0000001;
+		int otherNumber = 0;
+		double errorTest  = 0.0;
+		
+		try
+		{
+			testNumber = testNumber / otherNumber;
+		}
+		catch(ArithmeticException currentError)
+		{
+			JOptionPane.showMessageDialog(null, currentError.getMessage());
+			JOptionPane.showMessageDialog(null, currentError.getClass().toString());
+		}
+
+		try
+		{
+			errorTest = errorTest / errorTest;
+		}
+		catch(ArithmeticException currentError)
+		{
+			JOptionPane.showMessageDialog(null, currentError.getMessage());
+			JOptionPane.showMessageDialog(null, currentError.getClass().toString());
+		}
+		JOptionPane.showMessageDialog(null, "Funny answer: " + errorTest/errorTest);
+	}
+
+}
